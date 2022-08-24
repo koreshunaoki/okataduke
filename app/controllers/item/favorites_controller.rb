c07e9b1ae8d0616ad1c887e2259399d7386cfb0c
koreshunaoki <ks.i.ey.bkkyd@gmail.com>
2022-08-24 05:57:59 +0000

    作成

--- app/controllers/item/favorites_controller.rb
+++ app/controllers/item/favorites_controller.rb
@@ -2,7 +2,7 @@ class Item::FavoritesController < ApplicationController
   def create
     item = Item.find(params[:buy_id])
     favorite = current_customer.favorites.new(item_id: item.id)
-    favorite.save
+    fav
     redirect_to item_buy_path(item.id)
   end
 
