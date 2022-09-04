e50e17b2058b237b0951d4924bf3272de5d1f9c5
koreshunaoki <ks.i.ey.bkkyd@gmail.com>
2022-09-04 15:45:37 +0000

    作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :regis
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :last_name, presence: true
