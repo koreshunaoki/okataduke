5dd2d4d7c95a6c2d0c04eb44920faedd7df7e5a8
koreshunaoki <ks.i.ey.bkkyd@gmail.com>
2022-09-15 16:09:32 +0000

    作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(resource)
-    item_buys_path
+  def after_sign_i
   end
 
   def configure_permitted_parameters
